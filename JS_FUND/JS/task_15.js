bc2603a35b6f21da73c9d38822277968c5582599
roskata729 <roskata728@gmail.com>
2021-04-23 19:46:29 +0300

    Added JS Task 15

---
+++ JS_FUND/JS/task_15.js
@@ -0,0 +1,6 @@
+/*
+    15.	Write a JavaScript function to test whether a string ends with a specified string.
+*/
+var myString = "This is my string";
+var endWord = "string";
+console.log(myString.endsWith(endWord));
