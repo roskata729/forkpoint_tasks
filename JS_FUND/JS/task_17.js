04bf55988df861ddff44c80e9f1649cb3713a3de
roskata729 <roskata728@gmail.com>
2021-04-23 20:04:33 +0300

    Added JS Task 17

---
+++ JS_FUND/JS/task_17.js
@@ -0,0 +1,24 @@
+/*
+    17.	Write a JavaScript function to get the first element of an array. Passing a parameter 'n' will return the first 'n' elements of the array.
+    Test Data:
+    console.log(first([7, 9, 0, -2]));
+    console.log(first([],3));
+    console.log(first([7, 9, 0, -2],3));
+    console.log(first([7, 9, 0, -2],6));
+    console.log(first([7, 9, 0, -2],-3));
+    Expected Output:
+    7
+    []
+    [7, 9, 0]
+    [7, 9, 0, -2]
+    []
+*/
+function first(myArray, n = 1){
+    var finalArray = myArray.slice(0, n);
+    return finalArray;
+}
+console.log(first([7, 9, 0, -2]));
+console.log(first([],3));
+console.log(first([7, 9, 0, -2],3));
+console.log(first([7, 9, 0, -2],6));
+console.log(first([7, 9, 0, -2],-3));
