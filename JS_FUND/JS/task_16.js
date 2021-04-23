eebdf0b1607233b4d60615c1839f10aac3a29476
roskata729 <roskata728@gmail.com>
2021-04-23 19:51:42 +0300

    Added JS Task 16

---
+++ JS_FUND/JS/task_16.js
@@ -0,0 +1,15 @@
+/*
+    16.	Write a JavaScript program to get the length of an JavaScript object.
+    Sample object:
+    var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12
+    };
+*/
+var student = {
+    name : "David Rayy",
+    sclass : "VI",
+    rollno : 12
+    };
+console.log("The length of the student's name is: " + student.name.length);
