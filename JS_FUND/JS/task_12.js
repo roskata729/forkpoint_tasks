da2b1a93c6761bbcb3148eca556c50e225ba21b3
roskata729 <roskata728@gmail.com>
2021-04-23 19:30:02 +0300

    Added JS task 12

---
+++ JS_FUND/JS/task_12.js
@@ -0,0 +1,7 @@
+/* 
+12.	Write a JavaScript function to remove the first occurrence 
+    of a given 'search string' from a string.
+*/
+var myString = "This is my string";
+var searchString = "my";
+console.log(myString.replace(searchString, ""));
