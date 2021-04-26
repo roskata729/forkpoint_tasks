13b8c527d9e18cc3625c476f4440aedbbca7de59
roskata729 <roskata728@gmail.com>
2021-04-26 18:24:47 +0300

    Added item and weapon classes

---
+++ JS_OOP/item.js
@@ -0,0 +1,23 @@
+class Item {
+
+    static countOfObjects = 0;
+    #name = '';
+    #id;
+
+    constructor (name){
+        this.#name = name;
+        id = ++countOfObjects;
+    }
+
+    getItemInfo(){
+        return 'Item ${this.id} - ${this.name}';
+    }
+
+    set name(name){
+        this.name = name;
+    }
+    get name(){
+        return this.name;
+    }
+
+}
---
+++ JS_OOP/weapon.js
@@ -0,0 +1,49 @@
+const allDamageTypes = ['psysical','poison','fire', 'water', 'air', 'earth'];
+class Weapon extends Item {
+    
+    #attack;
+    #twoHanded;
+    #damageType;
+    
+    constructor (name,attack, damageType, twoHanded) {
+        super(name);
+        this.attack = attack;
+        this.damageType = damageType;
+        this.twoHanded = twoHanded;
+        this.checkRequirements();
+    }
+
+    checkRequirements(){
+
+        if(this.attack < 1 && this.attack > 30000) throw 'The attack does NOT meet the requirements';
+        if(!allDamageTypes.includes(this.damageType)) throw 'The damage type is incorrect';
+        if(typeof this.twoHanded != 'boolean') throw 'Two-Handed is incorrect';
+    }
+
+    getItemInfo(){
+        return '${super.getItemInfo()} has ${this.attack} of ${this.damagetype}'
+    }
+
+    set attack(attack){
+        this.attack = attack;
+    }
+    get attack(){
+        return this.attack;
+    }
+
+    set twoHanded(twoHanded){
+        this.twoHanded = twoHanded;
+    }
+    get twoHanded(){
+        return this.twoHanded;
+    }
+
+    set damageType(damageType){
+        this.damageType = damageType;
+    }
+    get damageType(){
+        return this.damageType;
+    }
+
+
+}
